c8866c302efebdd8412d4cf4c0061c220802441d
yanhashi521 <hashiryu0521@gmail.com>
2023-02-08 17:54:07 +0900

    一旦googleマップをwebページ上に表示することができたところまで．マップを表示するコードはGoogleMap_show.jsxに記述してexportした

--- resources/js/Pages/Post/Show_local_map.jsx
+++ resources/js/Pages/Post/Show_local_map.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
 import { Link, router } from '@inertiajs/react';
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import GoogleMap_show from "./GoogleMap_show"
+
 
 const Show_local_map = (props) => {
     
+    const containerStyle = {
+        width: "1000px",
+        height: "600px",
+    };
+
+    const center = {
+        lat: 35.65818207396587, 
+        lng: 139.70166798416372,
+    }
+    
+    const positionShibuya = {
+        lat: 35.65818207396587, 
+        lng: 139.70166798416372,
+    }
+
     return (
         <Authenticated auth={props.auth} header={
                 <h2 className="font-semibold text-xl text-gray-800 leading-tight">
@@ -18,6 +36,9 @@ const Show_local_map = (props) => {
                         <input type="text" placeholder="渋谷駅"/>
                         <button type="submit" className="p-1 bg-purple-300 hover:bg-purple-400 rounded-md">検索</button>
                     </div>
+                    
+                    <GoogleMap_show/>
+                    
                 </div>
             </div>
             
